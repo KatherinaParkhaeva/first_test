733a9cb5ad1f671dfcb741a299625f307631b9b7
KatherinaParkhaeva <eparkhaeva@gmail.com>
2020-04-04 19:55:43 +0300

    Test_v2

--- src/test/java/ru/deliveryClub/FirstTest.java
+++ src/test/java/ru/deliveryClub/FirstTest.java
@@ -1,21 +1,42 @@
 package ru.deliveryClub;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstTest {
 
+    public ChromeDriver driver;
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "d://Users//user//Downloads//chromedriver.exe");
+        driver = new ChromeDriver();
+        System.out.println("Test start");
+    }
+
     @Test
     public void firstTest() {
-        System.setProperty("webdriver.chrome.driver", "d://Users//user//Downloads//chromedriver.exe");
-        ChromeDriver driver = new ChromeDriver();
+
         driver.get("https://www.delivery-club.ru/nn");
 
         String title = driver.getTitle();
         Assert.assertTrue(title.equals("Доставка еды из ресторанов Нижнего Новгорода от 45 минут! Delivery Club"));
+    }
 
+    @Test
+    public void firstTest2() {
+
+        driver.get("https://www.delivery-club.ru/nn");
+
+        String title = driver.getTitle();
+        Assert.assertTrue(title.equals("Доставка еды из ресторанов Нижнего Новгорода от 45 минут! Delivery Club"));
+    }
+
+    @After
+    public void Close() {
+        System.out.println("Test close");
         driver.quit();
-
     }
 }
